da8711610862c57cf1c0ff0c66d5b677e1c36dc4
vlean <vlean@qq.com>
2023-11-07 10:02:49 +0000

    d

--- api/oauth.go
+++ api/oauth.go
@@ -17,18 +17,25 @@ var (
 	authFail   = errors.New("fail client_id or host or token or redirect_uri")
 )
 
+type AuthReq struct {
+	ClientID     string `form::"client_id"`
+	ResponseType string `form:"response_type"`
+	State        string `form:"state"`
+	RedirectUri  string `form:"redirect_uri"`
+	Scope        string `form:"scope"`
+}
+
 func Auth(ctx *gin.Context) (data any, err error) {
-	client := ctx.Query("client_id")
-	if client == "" || !strings.HasPrefix(client, "oauth-") {
+	req, err := Bind[AuthReq](ctx)
+	if err != nil {
+		return
+	}
+	if req.ClientID == "" || !strings.HasPrefix(req.ClientID, "oauth-") || req.RedirectUri == "" {
 		err = authFail
 		return
 	}
-	ri := ctx.Query("redirect_uri")
-	if ri == "" {
-		err = authFail
-		return
-	}
-	toUrl, err := url.Parse(ri)
+
+	toUrl, err := url.Parse(req.RedirectUri)
 	if err != nil {
 		return
 	}
@@ -41,21 +48,31 @@ func Auth(ctx *gin.Context) (data any, err error) {
 	}
 	tk = tk[:32]
 
-	tokenCache.Range(func(key, value any) bool {
-		vt, ok := value.(time.Time)
-		if !ok {
-			return true
-		}
-		if time.Since(vt) > time.Minute*10 {
-			tokenCache.Delete(key)
-		}
-		return true
-	})
+	// tokenCache.Range(func(key, value any) bool {
+	// 	vt, ok := value.(time.Time)
+	// 	if !ok {
+	// 		return true
+	// 	}
+	// 	if time.Since(vt) > time.Minute*10 {
+	// 		tokenCache.Delete(key)
+	// 	}
+	// 	return true
+	// })
 	tokenCache.Store(tk, time.Now())
 
 	q := toUrl.Query()
 	q.Add("code", tk)
-	q.Add("state", ctx.GetString("state"))
+	q.Add("client_id", req.ClientID)
+	if req.State != "" {
+		q.Add("state", req.State)
+	}
+	if req.ResponseType != "" {
+		q.Add("resposne_type", req.ResponseType)
+	}
+	if req.Scope != "" {
+		q.Add("scope", req.Scope)
+	}
+
 	toUrl.RawQuery = q.Encode()
 	ctx.Redirect(http.StatusTemporaryRedirect, toUrl.String())
 	return
